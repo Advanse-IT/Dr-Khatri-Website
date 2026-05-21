6d7b7dea0b1f5f6411659c7d9c5d9d3f79da43f2
Sush-HD <143142919+Sush-HD@users.noreply.github.com>
2026-05-21 18:24:16 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+
 export default defineConfig({
-  build: {
-    outDir: "dist"
-  }
+  plugins: [cloudflare()]
 });
