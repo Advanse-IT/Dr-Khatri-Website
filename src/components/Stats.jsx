da68a6dbbc5f95e9e8bfbf6f3b76cf8ef8436b4c
Sush-HD <1.43142919e+08+Sush-HD@users.noreply.github.com>
2026-05-23 12:01:02 +0000

    Replace unprofessional heart watermarks with professional anatomical heart SVG

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,7 +8,7 @@ const markup = `
 <path d="M0,30 L140,30 L156,30 L161,28 L166,6 L171,54 L176,18 L181,30 L300,30 L316,30 L321,28 L326,6 L331,54 L336,18 L341,30 L460,30 L476,30 L481,28 L486,6 L491,54 L496,18 L501,30 L620,30 L636,30 L641,28 L646,6 L651,54 L656,18 L661,30 L780,30 L796,30 L801,28 L806,6 L811,54 L816,18 L821,30 L940,30 L956,30 L961,28 L966,6 L971,54 L976,18 L981,30 L1100,30 L1260,30 L1440,30" fill="none" stroke="white" stroke-linecap="round" stroke-width="1.5"></path>
 </svg>
 </div>
-<svg class="stats-heart" viewbox="0 0 200 180"><path d="M100 158C100 158 18 108 18 58C18 33 38 13 63 13C79 13 91 22 100 34C109 22 121 13 137 13C162 13 182 33 182 58C182 108 100 158 100 158Z"></path></svg>
+<img src="/images/anatomical-heart.svg" class="stats-heart" style="position:absolute;width:300px;right:-50px;bottom:-20px;opacity:0.08;filter:brightness(0) invert(1);pointer-events:none" alt="" />
 <div class="wrap" style="position:relative;z-index:2">
 <div class="stats-top">
 <div>
